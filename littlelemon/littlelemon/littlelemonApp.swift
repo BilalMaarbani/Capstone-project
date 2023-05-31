e87a6c66a7cfebea3557b3c24d6aa8d0e92148a5
Bilal Maarbani <bilall.maarbanii@gmail.com>
2023-05-31 11:48:21 +0300

    new project impl

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Bilal Maarbani on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
